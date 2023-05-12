3d90da6ff36b1d4526502160a8eede95ec2eb314
Martha Ochoa <marthaeochoa98@gmail.com>
2023-05-12 18:40:04 -0500

    add styling

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -5,10 +5,12 @@ export const PostItem = ({post, param, blogId}) => {
     
     function loadPosts(){
         if(parseInt(param) === blogId){
-            return <ul>
-            <li>{title}</li>
+            return <div> <ul className='cards'>
+            <li className='title'><h4>{title}</h4></li>
             <li>{description}</li>
+            <button>See comments</button>
         </ul>
+        </div>
         } else {
             return null
         }
